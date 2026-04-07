ae3588251a1251fb07099d3979aaf2964300750a
Sergio Gomez <sergio@Sergios-MacBook-Pro.local>
2026-04-07 21:17:02 +0200

    Prepare the dashboard to be deployed

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from routes import register_routes
 
 from services.telegram_service import start_scheduler
 
+from dotenv import load_dotenv
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +12,7 @@ def create_app():
     start_scheduler()
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=3062, debug=True)
